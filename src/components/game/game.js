5a57ad50e36d08a3b92eb039bac90b36bd13c483
Bryan <bryan.garcia.cuevas@gmail.com>
2023-09-18 11:39:35 -0700

    added movement to player

---
+++ src/components/game/game.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './game.css'
+import $ from 'jquery';
+
+export default function Game() {
+
+  var player = $('#player'),
+  block = $('#block'),
+  wh = player.width() - block.width(),
+  wv = player.height() - block.height(),
+  d = {},
+  x = 5;
+
+  function newh(v,a,b) {
+      var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+      return n < 0 ? 0 : n > wh ? wh : n;
+  }
+
+  function newv(v,a,b) {
+      var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+      return n < 0 ? 0 : n > wv ? wv : n;
+  }
+
+
+  $(window).keydown(function(e) { d[e.which] = true; });
+  $(window).keyup(function(e) { d[e.which] = false; });
+
+  setInterval(function() {
+      block.css({
+          left: function(i,v) { return newh(v, 37, 39); },
+          top: function(i,v) { return newv(v, 38, 40); }
+      });
+      wh = player.width() - block.width();
+      wv = player.height() - block.height();
+  }, 20);
+
+
+
+
+  return (
+    <div className ="container">
+        <div className="game">
+          <div className='player' id='player'>
+            <div className='block' id='block'></div>
+          </div>
+          <div className='ball-area'>
+            <div className='ball'></div>
+          </div>
+          <div className='enemy'>
+            <div className='block'></div>
+          </div>
+        </div>
+    </div>
+  )
+}
